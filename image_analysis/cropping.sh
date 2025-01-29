baef860a44dd8b7139c16dbd6d82c9e575ba334c
nquintanaparrilla <139363152+nquintanaparrilla@users.noreply.github.com>
2025-01-29 12:43:38 -0800

    Create cropping.sh

---
+++ image_analysis/cropping.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH --gres=gpu:1
+#SBATCH --time=8:00:00
+
+module load miniconda3
+eval "$(conda shell.bash hook)" 
+conda activate cellpose-env
+srun python cropping.py
